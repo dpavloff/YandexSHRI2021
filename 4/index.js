3c13be98059c734177f2ffee2185e99111d39d91
pavlov.dan <pavlov.dan@panoramik.ru>
2021-09-10 21:37:36 +0500

    6/10 tests pass

--- 4/index.js
+++ 4/index.js
@@ -1,22 +1,24 @@
-let costsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const tel = (n, costsArray) => {
-    let res = [];
-    let del = 1;
-    
-    if (costsArray.length < 1 || !costsArray.find((e) => e < n) || n === 0) {
-      return "";
+function tel(money, costsArray) {
+    let result = 0;
+
+    if (costsArray.length < 1 || money === 0 || !costsArray.find(e => e < money)) {
+        return "";
     }
-    
-    while (n / del > 1) {
-        let num = (n / del) % 10 | 0;
-        let min = n;
-        costsArray.forEach(e => {
-          if (num - e >= 0) {
-            min = Math.min(min, num - e);
-          }
-        });
-        del *= 10;
-        res.push(costsArray.lastIndexOf(min + num));
-      }
-    return res.reverse().join("");
+
+    costsArray.forEach((element, index) => {
+        if (result < index++ && element < money && Number.isInteger(money / element)) {
+            result = index++
+        }
+    });
+
+
+
+    // let result = costsArray.find(perfectCost);
+    // (x < money) && (x / money % 10 === 0););
+
+    let finalIndex = result - 1;
+
+    return result.toString().repeat(money / costsArray[finalIndex]);
 };
+
+console.log(tel(0,[1,1,1,1,1,1,1,1,1]));
